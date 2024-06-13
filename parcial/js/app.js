44f4cb10ef147c48fb71f7ca0d3bbb25b55206e7
mateoburellab <112600174+mateoburellab@users.noreply.github.com>
2024-06-13 18:18:09 -0300

    Add files via upload

---
+++ parcial/js/app.js
@@ -0,0 +1,39 @@
+//Ejercicio 1
+const productos = [
+    {
+        nombre: "Martillo",
+        descripcion: "Martillo de acero con mango de madera.",
+        precio: 15.99,
+        cantidad: 50
+    },
+    {
+        nombre: "Destornillador",
+        descripcion: "Destornillador Phillips de 6 pulgadas.",
+        precio: 5.99,
+        cantidad: 150
+    },
+    {
+        nombre: "Taladro",
+        descripcion: "Taladro inalámbrico con batería recargable.",
+        precio: 99.99,
+        cantidad: 30
+    },
+    {
+        nombre: "Cinta Métrica",
+        descripcion: "Cinta métrica de 5 metros.",
+        precio: 3.99,
+        cantidad: 200
+    },
+    {
+        nombre: "Sierra",
+        descripcion: "Sierra manual de 12 pulgadas.",
+        precio: 10.99,
+        cantidad: 75
+    }
+];
+
+
+//Ejercicio 2
+
+
+//Ejercicio 3
